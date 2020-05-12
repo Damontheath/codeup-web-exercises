1a286af49aa4ff483f0712df430d77257ad5ff30
Damon Heath <damon.t.heath@gmail.com>
2020-05-12 14:17:40 -0500

    exc

---
+++ js/jquery-selectors.js
@@ -0,0 +1,15 @@
+$('body').css({
+    'background-color': 'black',
+    'color': 'white'
+});
+
+$('li').css('font-size', '20px');
+
+$('h1,p,li').css({
+    'background-color': "white",
+    'color': 'black'
+});
+
+var popUp =  $('h1').html();
+
+alert(popUp);
